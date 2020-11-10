a330352c16579432da6dd844f60f4b7f15557ef0
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-11-10 17:30:22 -0500

    Update new_connection.php

--- Website Code/Student Pages/new_connection.php
+++ Website Code/Student Pages/new_connection.php
@@ -1,5 +1,3 @@
-<html>
-
 <?php
 
 //Connecting to database
@@ -13,19 +11,6 @@ $data_base = mysqli_connect($servername, $username, $password, $dbname);
 if(!$data_base){
 die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected Successfully!";
+// echo "Connected Successfully!";
 
-$sql = "SELECT * FROM Student WHERE Email = '$Email'"
-$result = mysqli_query($data_base,$sql);
-if (mysqli_num_rows($result) > 0){
-	while ($row = mysqli_fetch_assoc($result)){
-		echo .$row["Fname"]. .$row["Lname"]. "Student Profile"<br>
-	}
-}else{
-	echo "Student not found";
-}
-
-mysqli_close($data_base);
 ?>
-
-</html>
