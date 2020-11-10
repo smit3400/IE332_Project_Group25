5dc12765a3273ab8c1fe0f0b20955033b4239dd9
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-11-10 17:30:00 -0500

    Update student_form.php

--- Website Code/Student Pages/student_form.php
+++ Website Code/Student Pages/student_form.php
@@ -7,102 +7,105 @@
 
 </body>
 
-<form method="post" action="#">
-	<label for="img">Select profile image:</label>
-	<input type="file" id="img" name="img" accept="image/*">
-	<div>&nbsp;
-	
-	<div class="form-group"><label for="Email">Email Address</label> <input id="Email" class="form-control" type="email" placeholder="Enter email" aria-describedby="Email" />
-	<div>&nbsp;
-	
-	<div class="form-group"><label for="Password">Password</label> <input id="Password" class="form-control" type="password" placeholder="Enter password" aria-describedby="Password" />
-	<div>&nbsp;
-	
-	<div class="form-group"><label for="Fname">First Name</label> <input id="Fname" class="form-control" type="text" placeholder="Enter first name" aria-describedby="Fname" />
-	<div>&nbsp;
-  
-	<div class="form-group"><label for="Lname">Last Name</label> <input id="Lname" class="form-control" type="text" placeholder="Enter last name" aria-describedby="Lname" />
-	<div>&nbsp;
+<?php
+include "new_connection.php"; // use database connection
 
-	<div class="form-group"><label for="Phone_Number">Phone Number (area code first) </label> <input id="Phone_Number" class="form-control" type="tel" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" placeholder="1234567890" />
-	<div>&nbsp;
-  
-	<div class="form-group"><label for="Major">Major</label>
-		<select id="Major" class="form-control">
+if (isset($_POST['submit'])) {
+	$Email = $_POST['Email'];
+	$Password = $_POST['Password'];
+	$Fname = $_POST['Fname'];
+	$Lname = $_POST['Lname'];
+	$Phone_Number = $_POST['Phone_Number'];
+	$Major = $_POST['Major'];
+	$Location = $_POST['Location'];
+	$GPA = $_POST['GPA'];
+	$Experience = $_POST['Experience'];
+	$Courses = $_POST['Courses'];
+	$Year = $_POST['Year'];
+	$Opportunity_Type = $_POST['Opportunity_Type'];
+	$Relocation = $_POST['Relocation'];
+	$Work_Sponsorship = $_POST['Work_Sponsorship'];
+
+	$insert = mysqli_query($data_base, "INSERT INTO Student (Email, Password, Fname, Lname, Phone_Number, Major, Location, GPA, Experience, Courses, Year, Opportunity_Type, Relocation, Work_Sponsorship) VALUES ('$Email', '$Password', '$Fname', '$Lname', '$Phone_Number', '$Major', '$Location', '$GPA', '$Experience', '$Courses', '$Year', '$Opportunity_Type', '$Relocation', '$Work_Sponsorship')");
+
+	if(!$insert){
+		echo mysqli_error();
+	}
+	else{
+		echo "Records added successfully!";
+	}
+}
+mysqli_close($data_base); //close connection
+?>
+
+<form method="POST">
+	
+	Email Address: <input type = "email" name = "Email" placeholder = "Enter email address" Required>
+	<br/>
+	<br/>
+	Password: <input type = "password" name = "Password" placeholder = "Enter password" Required>
+	<br/>
+	<br/>
+	First Name: <input type = "text" name = "Fname" placeholder = "Enter first name" Required>
+	<br/>
+	<br/>
+	Last Name: <input type = "text" name = "Lname" placeholder = "Enter last name" Required>
+	<br/>
+	<br/>
+	Phone Number (area code first): <input type = "tel" pattern = "[0-9]{3}[0-9]{3}[0-9]{4}" placeholder = "1234567890" Required>
+	<br/>
+	<br/>
+	Major:
+	<select type = "text" name="Major">
 		<option>FYE</option>
 		<option>IE</option>
-		</select>
-	<div>&nbsp;
-  
-	<div class="form-group"><label for="Location">Location Preference </label>
-	<select id="Location" class="form-control">
+	</select>
+	<br/>
+	<br/>
+	Location Preference: 
+	<select type = "text" name = "Location">
 		<option>Northeast</option>
 		<option>South</option>
 		<option>Midwest</option>
 		<option>West</option>
 	</select>
+	<br/>
+	<br/>
+	GPA: <input type="text" name = "GPA" placeholder="Enter GPA" Required>
+	<br/>
+	<br/>
+	Technical Skills (no commas, e.g. python CAD java): <input type = "text" name = "Experience" placeholder="Enter technical skills" Required>
+	<br/>
+	<br/>
+	Courses (only relevant to IE and/or FYE, no commas, e.g. IE230 IE343 IE270): <input type="text" name = "Courses" placeholder="Enter courses" Required>
 	<div>&nbsp;
-  
-	<div class="form-group"><label for="GPA">GPA </label> <input id="GPA" class="form-control" type="text" placeholder="Enter GPA" />
-	<div>&nbsp;
-  
-	<div class="form-group"><label for="Experience">Technical Skills (no commas, e.g. python CAD java)</label> <input id="Experience" class="form-control" type="text" placeholder="Enter experience" />
-	<div>&nbsp;
-  
-	<div class="form-group"><label for="Courses">Courses (only relevant to IE and/or FYE, no commas, e.g. IE230 IE343 IE270)</label> <input id="Courses" class="form-control" type="text" placeholder="Enter courses" />
-	<div>&nbsp;
-  
-	<div class="form-group"><label for="Year">Year (1,2,3, etc.)</label> <input id="Year" class="form-control" type="text" placeholder="Enter year" />
-	<div>&nbsp;  
-
-	<div class="form-group"><label for="Opportunity_Type">Oportunity Type </label>
-	<select id="Opportunity_Type" class="form-control">
+	<br/>
+	<br/>
+	Year (1,2,3, etc.): <input type="text" name = "Year" placeholder="Enter year" Required>
+	<br/>
+	<br/>
+	Oportunity Type:
+	<select type = "text" name = "Opportunity_Type">
 		<option>Internship</option>
 		<option>Co-op</option>
 		<option>Full Time</option>
 	</select>
-	<div>&nbsp;
-
-	<div class="form-group"><label for="Relocation">Willing to relocate? </label>
-	<select id="Relocation" class="form-control">
+	<br/>
+	<br/>
+	Willing to relocate?
+	<select type = "text" name = "Relocation">
 		<option>Yes</option>
 		<option>No</option>
 	</select>
-	<div>&nbsp;
-
-	<div class="form-group"><label for="Work_Sponsorship">Will you now or in the future require visa sponsorship for employment? </label>
-	<select id="Work_Sponsorship" class="form-control">
+	<br/>
+	<br/>
+	Will you now or in the future require visa sponsorship for employment?
+	<select type = "text" name = "Work_Sponsorship">
 		<option>Yes</option>
 		<option>No</option>
 	</select> 
-	<div>&nbsp;
-
+	<br/>
+	<br/>
 	<input type="submit" value="Create Account" />
-
 </form>
-
-<?php
-session_start();
-include 'new_connection.php';
-if (isset($_POST['submit'])) {
-$Email = $_POST['Email'];
-$Password = $_POST['Password'];
-$Fname = $_POST['Fname'];
-$Lname = $_POST['Lname'];
-$Phone_Number = $_POST['Phone_Number'];
-$Major = $_POST['Major'];
-$Location = $_POST['Location'];
-$GPA = $_POST['GPA'];
-$Experience = $_POST['Experience'];
-$Courses = $_POST['Courses'];
-$Year = $_POST['Year'];
-$Opportunity_Type = $_POST['Opportunity_Type'];
-$Relocation = $_POST['Relocation'];
-$Work_Sponsorship = $_POST['Work_Sponsorship'];
-$query = "INSERT INTO Student (Email, Password, Fname, Lname, Phone_Number, Major, Location, GPA, Experience, Courses, Year, Opportunity_Type, Relocation, Work_Sponsorship) VALUES ('".$Email."', '".$Password."', '".$Fname."', '".$Lname."', '".$Phone_Number."', '".$Major."', '".$Location."', '".$GPA."', '".$Experience."', '".$Courses."', '".$Year."', '".$Opportunity_Type."', '".$Relocation."', '".$Work_Sponsorship."')";
-$result = mysqli_query($data_base,$query)or die ('Error in updating Database');
-}
-
-?>
-
 </html>
