ff55607478848ebd1e6f80ade68256f6fda916ab
puffins <li3432@purdue.edu>
2020-11-12 17:43:36 -0500

    ie form recreated and able to submit to databse

---
+++ Website Code/Purdue IE Pages/IE_Form_Page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p></p>
+<h1>IE Advisor Profile Setup</h1>
+<p>Welcome! Please enter all relevant information to create your Advisor profile page.</p>
+
+<form action="welcome.php" method="get">
+First Name: <input type="text" name="fname"><br>
+Last Name: <input type="text" name="lname"><br>
+E-mail: <input type="text" name="email"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+</body>  
+</html>
---
+++ Website Code/Purdue IE Pages/welcome.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<body>
+	Welcome <?php echo $_GET["fname"]; ?><br>
+	Your email address is: <?php echo $_GET["email"]; ?> <br>
+	<?php
+
+	echo "Database Connection" . "<br>" . "<br>";
+	//Connecting to database
+	$servername = "mydb.itap.purdue.edu";
+	$username = "g1116905";
+	$password = "iegroup25";
+	$dbname = "g1116905";
+
+	$data_base = mysqli_connect($servername, $username, $password, $dbname);
+
+	if (mysqli_connect_errno()) {
+	    printf("Connect failed: %s\n", mysqli_connect_error());
+	    exit();
+	}else{
+		echo "Connected Successfully!" . "<br>";
+	}
+
+	echo "Inserting Data". "<br>" . "<br>";
+
+	$advisoremail = $_GET["email"];
+	$advisorpass = $_GET["password"];
+	$afirst = $_GET["fname"];
+	$alast = $_GET["lname"];
+
+	$sql = "INSERT INTO Purdue_IE (Email, Password, Fname, Lname)
+	VALUES('" . $advisoremail . "', '" . $advisorpass . "', '" . $afirst . "', '" . $alast . "')";
+
+	if (mysqli_query($data_base, $sql)) {
+	  echo "New record created successfully";
+	} else {
+	  echo "Error: " . $sql . "<br>" . mysqli_error($data_base);
+	}
+
+	mysqli_close($data_base);
+
+	?>
+
+
+
+</body>
+</html>
