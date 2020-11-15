839e3277d4fd294322ed2b3f1755242f6940e00d
smit3400 <56548125+smit3400@users.noreply.github.com>
2020-11-15 15:30:45 -0500

    Update matchPlotStudents.R
    functional

--- Matching Algorithm Code R/matchPlotStudents.R
+++ Matching Algorithm Code R/matchPlotStudents.R
@@ -1,11 +1,16 @@
-matchPlots <- function(Student = NULL, scores = data.frame(), mydb)
+matchPlotStudents <- function(Student = NULL, scores = data.frame(), mydb)
 {
-    studentInfo <- subset.data.frame(scores, Email == Student)
-    # query <- paste("SELECT * FROM Match_Score WHERE Email =","'",Student,"'")
-    # print(query)
-    # studentInfo <- dbGetQuery(mydb, query)
-    print(studentInfo)
-    barplot(height = studentInfo$Weight_Score, names = studentInfo$Opportunity_ID,ylim=c(0,120),ylab = "Weighted Score", xlab = "Job Posting", main = paste("Weighted Scores Across All jobs for",Student))
-    box()
+
+
+Student <- "email@student.com"
     
-}
+query <- sprintf("SELECT *
+FROM `Match_Score`
+WHERE `Email` = '%s'
+LIMIT 0 , 10",Student)
+    query <- str_replace_all(str_replace_all(query,"\n",""),"\\s+"," ")
+    print(query)
+    studentInfo <- dbGetQuery(mydb, query)
+    barplot(height = studentInfo$Weight_Score, names = studentInfo$Opportunity_ID,ylim=c(0,120),ylab = "Weighted Score", xlab = "Job Posting", main = paste("Weighted Scores Across 10 jobs for",Student))
+    box()
+    }
