df2aa5131555dca659c6399c2233b680e8657faa
smit3400 <56548125+smit3400@users.noreply.github.com>
2020-11-17 15:58:05 -0500

    Update matchPlotStudents.R

--- Matching Algorithm Code R/matchPlotStudents.R
+++ Matching Algorithm Code R/matchPlotStudents.R
@@ -1,8 +1,7 @@
+require("plotrix")
+library(plotrix)
 matchPlotStudents <- function(Student = NULL, scores = data.frame(), mydb)
 {
-
-
-Student <- "email@student.com"
     
 query <- sprintf("SELECT *
 FROM `Match_Score`
@@ -13,4 +12,33 @@ LIMIT 0 , 10",Student)
     studentInfo <- dbGetQuery(mydb, query)
     barplot(height = studentInfo$Weight_Score, names = studentInfo$Opportunity_ID,ylim=c(0,120),ylab = "Weighted Score", xlab = "Job Posting", main = paste("Weighted Scores Across 10 jobs for",Student))
     box()
-    }
+    
+    #avg weighted score for job postings of each skill
+progQ<-sprintf("SELECT AVG( Weight_Score )
+    FROM Match_Score M, Opportunities O
+    WHERE M.Opportunity_ID = O.Opportunity_ID
+    AND Skill = 'Programming'
+    AND M.Email = '%s'",Student)
+progQ <- str_replace_all(str_replace_all(progQ,"\n",""),"\\s+"," ")
+progScore <- dbGetQuery(mydb,progQ)
+
+StatQ<-sprintf("SELECT AVG( Weight_Score )
+    FROM Match_Score M, Opportunities O
+    WHERE M.Opportunity_ID = O.Opportunity_ID
+    AND Skill = 'Statistics'
+    AND M.Email = '%s'",Student)
+StatQ <- str_replace_all(str_replace_all(StatQ,"\n",""),"\\s+"," ")
+StatScore <- dbGetQuery(mydb,StatQ)
+
+techQ<-sprintf("SELECT AVG( Weight_Score )
+    FROM Match_Score M, Opportunities O
+    WHERE M.Opportunity_ID = O.Opportunity_ID
+    AND Skill = 'Technical Design'
+    AND M.Email = '%s'",Student)
+techQ <- str_replace_all(str_replace_all(techQ,"\n",""),"\\s+"," ")
+techScore <- dbGetQuery(mydb,techQ)
+    
+    skillScores <- data.frame(Programming = progScore[1,], Statistics = StatScore[1,], Technical_Design =techScore[1,])
+    barplot(height = as.numeric(skillScores[1,]), names.arg = names(skillScores), ylim=c(0,100),ylab = "Average Weighted Score", xlab = "Skill", main = "Average Score for each skill across all postings")
+    box()
+}
