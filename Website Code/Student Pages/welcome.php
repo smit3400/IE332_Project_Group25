514c7dccff5e634d72b5b5f9eee78c59257b1f24
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-11-17 16:07:06 -0500

    Update welcome.php

--- Website Code/Student Pages/welcome.php
+++ Website Code/Student Pages/welcome.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-	Welcome <?php echo $_GET["Fname"]; ?>!<br>
+	Welcome <?php echo $_POST["Fname"]; ?>!<br>
 	
 	<?php
 
@@ -19,20 +19,20 @@
 	    exit();
 	}
 	
-	$stud_email = $_GET['Email'];
-	$stud_password = $_GET['Password'];
-	$stud_fname = $_GET['Fname'];
-	$stud_lname = $_GET['Lname'];
-	$stud_phone = $_GET['Phone_Number'];
-	$stud_major = $_GET['Major'];
-	$stud_location = $_GET['Location'];
-	$stud_GPA = $_GET['GPA'];
-	$stud_experience = $_GET['Experience'];
-	$stud_courses = $_GET['Courses'];
-	$stud_year = $_GET['Year'];
-	$stud_opp = $_GET['Opportunity_Type'];
-	$stud_reloction = $_GET['Relocation'];
-	$stud_sponsorship = $_GET['Work_Sponsorship'];
+	$stud_email = $_POST['Email'];
+	$stud_password = $_POST['Password'];
+	$stud_fname = $_POST['Fname'];
+	$stud_lname = $_POST['Lname'];
+	$stud_phone = $_POST['Phone_Number'];
+	$stud_major = $_POST['Major'];
+	$stud_location = $_POST['Location'];
+	$stud_GPA = $_POST['GPA'];
+	$stud_experience = $_POST['Experience'];
+	$stud_courses = $_POST['Courses'];
+	$stud_year = $_POST['Year'];
+	$stud_opp = $_POST['Opportunity_Type'];
+	$stud_reloction = $_POST['Relocation'];
+	$stud_sponsorship = $_POST['Work_Sponsorship'];
 
 	$check = mysqli_query($data_base, "SELECT * FROM Student WHERE Email = '$stud_email'");
 	if(mysqli_num_rows($check) > 0){
