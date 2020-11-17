48ab33fbd73d09362e295d36f7a5aa43e45ea208
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-11-17 16:08:42 -0500

    Create student_main.php

---
+++ Website Code/Student Pages/student_main.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<h5> <hr> <Strong> Student </Strong> </h5>
+<p><a href = "student_profile.php">Profile Page</a></p>
+<p><a href = "student_compratings.php">Company Ratings</a></p>
+<p><a href = "student_jobfinder.php">Job Finder</a></p>
+<p><a href = "student_jobmatch.php">Job Match</a></p>
+<p><a href = "student_logout.php">Logout</a></p>
+</body>
+</html>
