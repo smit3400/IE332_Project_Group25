eea25af1e42479868201b8f4a957a6b71356fc0b
smit3400 <56548125+smit3400@users.noreply.github.com>
2020-11-24 11:38:26 -0500

    Update matchingInputs.R

--- Matching Algorithm Code R/matchingInputs.R
+++ Matching Algorithm Code R/matchingInputs.R
@@ -2,11 +2,16 @@ require("RMySQL")
 library(RMySQL)
 install.packages("data.table")
 library(data.table)
+require("stringr")
+library(stringr)
 args <- commandArgs(TRUE)
 
 ##Connect to the database
 mydb <- dbConnect(MySQL(), user = "g1116905", password = "iegroup25", dbname = "g1116905", host = "mydb.itap.purdue.edu")
 on.exit(dbDisconnect(mydb))
+
+source('~/R/matching.R')
+source('~/R/matchPlotStudents.R')
 
 
 ##Initialize student data and all opportunities data into dataframes
@@ -17,10 +22,13 @@ i<-1
 matchTable <- data.frame("Email" = character(), "Opportunity_ID"  = integer(), "Match_Score" = integer(),"Weight_Score" = integer())
 while(i<=length(sData$Email))
 {
-  temp <- matching(sData = sData[i,], oData = oData)
+  temp <- matching(sData = sData[i,], oData = oData, mydb = mydb)
   matchTable <- rbind(matchTable,temp)
   i<-i+1
 }
 
 dbWriteTable(mydb, "Match_Score", matchTable, overwrite = TRUE, row.names = FALSE)
+
+matchPlotStudents(Student = "email@student.com",scores = matchTable, mydb = mydb)
+
 
