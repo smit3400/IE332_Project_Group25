e6a19141e0e124a426193230c6e6b4527d9fd4e0
Eduardo Aramayo <71730587+earamayof@users.noreply.github.com>
2020-11-24 16:26:01 -0600

    Update student_jobfinder.php

--- Website Code/Student Pages/student_jobfinder.php
+++ Website Code/Student Pages/student_jobfinder.php
@@ -22,19 +22,28 @@ session_start();
   }
 
   //Pulling info from sql
-  $sql = "SELECT O.*, Weight_Score FROM Match_Score M, Opportunities O WHERE M.Opportunity_ID = O.Opportunity_ID AND M.Email='" . $_SESSION["stud_email"] . "' ORDER BY Weight_Score DESC";
+  $sql = "SELECT O.*, Weight_Score, Company_Name FROM Match_Score M, Opportunities O, Employer WHERE M.Opportunity_ID = O.Opportunity_ID AND M.Email='" . $_SESSION["stud_email"] . "' ORDER BY Weight_Score DESC";
   $result = mysqli_query($data_base,$sql);
 
   if (mysqli_num_rows($result) > 0) {
-    echo "<table>";
-    echo "<tr>";
-    echo "<td>Score</td>";
-    echo "</tr>";
+    echo "<table border = '1'>
+    <tr>
+    <th>Opportunity ID</th>
+    <th>Company Name</th>
+    <th>Job Title</th>
+    <th>Opportunity Type</th>
+    <th>Job Description</th>
+    <th>Matching Score</th>
+    </tr>
+    ";
     while ($row = mysqli_fetch_array($result)) {
       echo "<tr>";
-      echo "<td>";
-      echo $row["Weight_Score"];
-      echo "</td>";
+      echo "<td>" . $row["Opportunity_ID"] . "</td>";
+      echo "<td>" . $row["Company_Name"] . "</td>";
+      echo "<td>" . $row["Job_Title"] . "</td>";
+      echo "<td>" . $row["Opportunity_Type"] . "</td>";
+      echo "<td>" . $row["Job_Description"] . "</td>";
+      echo "<td>" . $row["Weight_Score"] . "</td>";
       echo "</tr>";
     }
   } else {
