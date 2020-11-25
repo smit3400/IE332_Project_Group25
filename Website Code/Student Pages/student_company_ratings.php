fafd29494e34c08a7e93653939f2729c45ac65af
Claired1071 <71730721+Claired1071@users.noreply.github.com>
2020-11-25 18:51:06 -0500

    Create student_company_ratings.php

---
+++ Website Code/Student Pages/student_company_ratings.php
@@ -0,0 +1,60 @@
+<!--?php 
+session_start();
+?-->
+<h1>Rate The Company</h1>
+<p>Hello! Please enter all relevant information to rate this company.</p>
+<form action="student_company_ratings.php" method="get">
+<div>&nbsp;</div>
+Friendliness:<select name="friendliness">
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+<option value="10">10</option>
+</select><br />
+<div>&nbsp;</div>
+Flexibility:<select name="flexibility">
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+<option value="10">10</option>
+</select><br />
+<div>&nbsp;</div>
+Transparency:<select name="transparency">
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+<option value="10">10</option>
+</select><br />
+<div>&nbsp;</div>
+Diversity:<select name="diversity">
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+<option value="10">10</option>
+</select>
+<div>&nbsp;</div>
+<input type="submit" /></form>
