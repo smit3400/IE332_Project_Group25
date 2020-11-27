5f9727061b38895706a1a2f5f42b5a3222553746
smit3400 <56548125+smit3400@users.noreply.github.com>
2020-11-27 11:32:46 -0500

    Update matchPlotsEmployers.R

--- Matching Algorithm Code R/matchPlotsEmployers.R
+++ Matching Algorithm Code R/matchPlotsEmployers.R
@@ -26,4 +26,15 @@ matchPlotEmployer <- function(mydb, Company)
                na.action = na.omit)
   predicted_kNN <- predict(model, newdata = testing)
   
+  Q2<-sprintf("SELECT `Opportunity_ID` , AVG( `Weight_Score` )
+  FROM `Match_Score`
+  GROUP BY `Opportunity_ID` ORDER BY AVG( `Weight_Score` ) DESC
+  LIMIT 0 , 10")
+  Q2 <- str_replace_all(str_replace_all(Q2,"\n",""),"\\s+"," ")
+  rankID <- dbGetQuery(mydb,Q2)
+  names(rankID)[names(rankID) == "AVG( `Weight_Score` )"] <- "AVG_Score"
+  barplot(height = rankID$AVG_Score, names.arg = rankID$Opportunity_ID, ylim = c(0,100) ,ylab = "Average Score", xlab = "Job Posting #", main = "Average Score Across Top 10 Postings for All Companies")
+  box()
+  
+    
 }
