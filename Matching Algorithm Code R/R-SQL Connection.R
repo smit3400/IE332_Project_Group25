3e41396bbbb08b2580ed0407b9cfcb638197b1a9
smit3400 <56548125+smit3400@users.noreply.github.com>
2020-11-28 11:23:02 -0500

    Delete R-SQL Connection.R

--- Matching Algorithm Code R/R-SQL Connection.R
+++
@@ -1,6 +0,0 @@
-require("RMySQL")
-library(RMySQL)
-args <- commandArgs(TRUE)
-
-mydb <- dbConnect(MySQL(), user = "g1116905", password = "iegroup25", dbname = "g1116905", host = "mydb.itap.purdue.edu")
-on.exit(dbDisconnect(mydb))
