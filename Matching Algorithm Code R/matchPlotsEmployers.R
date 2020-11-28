15d762e44dca1c1c8c2ee5f76faec2196a550284
smit3400 <56548125+smit3400@users.noreply.github.com>
2020-11-28 16:42:54 -0500

    Update matchPlotsEmployers.R

--- Matching Algorithm Code R/matchPlotsEmployers.R
+++ Matching Algorithm Code R/matchPlotsEmployers.R
@@ -1,8 +1,8 @@
-install.packages("caret")
+require("caret")
 library(caret)
 library(e1071)
 library(ROCR)
-matchPlotEmployer <- function(mydb, Company)
+matchPlotEmployer <- function(mydb)
 {
 
   Q1<-sprintf("SELECT O.* , AVG( Weight_Score )
@@ -15,16 +15,22 @@ matchPlotEmployer <- function(mydb, Company)
 
   OScores <- OScores[,-7]
   OScores <- OScores[,-7]
+  OScores <- OScores[,-1]
+  OScores <- OScores[,-1]
   names(OScores)[names(OScores) == "AVG( Weight_Score )"] <- "AVG_Score"
  #ML Algorithm - NB to predict average matchscore based off of job information
-  indexTrain <- createDataPartition(y = OScores$AVG_Score,p = 0.75,list = FALSE)
+  indexTrain <- createDataPartition(y = OScores$AVG_Score,p = 0.5,list = FALSE)
   training <- OScores[indexTrain,]
   testing <- OScores[-indexTrain,]
 
   model<-train(AVG_Score~., data = training,  method = "knn",
                preProcess = c('scale', 'center'),
                na.action = na.omit)
-  predicted_kNN <- predict(model, newdata = testing)
+  predicted_Weighted_Score <- predict(model, newdata = testing)
+  predictions <- cbind(testing,predicted_Weighted_Score)
+  predictions<- predictions[,-8]
+  
+ par(mfrow = c(2,1))
   
   Q2<-sprintf("SELECT `Opportunity_ID` , AVG( `Weight_Score` )
   FROM `Match_Score`
@@ -36,5 +42,12 @@ matchPlotEmployer <- function(mydb, Company)
   barplot(height = rankID$AVG_Score, names.arg = rankID$Opportunity_ID, ylim = c(0,100) ,ylab = "Average Score", xlab = "Job Posting #", main = "Average Score Across Top 10 Postings for All Companies")
   box()
   
-    
+  Q3 <- sprintf("SELECT `Statistics_Skill` , `Programming_Skill` , `Technical_Design_Skill`
+ FROM `Student")
+  sCounts<- dbGetQuery(mydb, Q3)
+  skills <- colnames(sCounts)[apply(sCounts,1,which.max)]
+  df <- data.frame(Statistics = sum(skills == "Statistics_Skill"), Programming = sum(skills == "Programming_Skill"), Design = sum(skills == "Technical_Design_Skill") )
+  pie(as.numeric(df[1,]), names(sCounts),main = "Student's Highest Ranking Skills")
+  
+  return(predictions)  
 }
