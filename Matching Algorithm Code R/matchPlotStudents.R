502b424bad542eab96df2562d7e8ca3e9e773880
smit3400 <56548125+smit3400@users.noreply.github.com>
2020-11-27 21:40:03 -0500

    Update matchPlotStudents.R

--- Matching Algorithm Code R/matchPlotStudents.R
+++ Matching Algorithm Code R/matchPlotStudents.R
@@ -1,10 +1,9 @@
 require("plotrix")
 library(plotrix)
 
-##Students = The desired student's email, scores = MatchScores Table, mydb = mydb
 matchPlotStudents <- function(Student = NULL, mydb)
 {
-    
+
 query <- sprintf("SELECT *
 FROM `Match_Score`
 WHERE `Email` = '%s'
@@ -12,8 +11,7 @@ LIMIT 0 , 10",Student)
     query <- str_replace_all(str_replace_all(query,"\n",""),"\\s+"," ")
     print(query)
     studentInfo <- dbGetQuery(mydb, query)
-    barplot(height = studentInfo$Weight_Score, names = studentInfo$Opportunity_ID,ylim=c(0,120),ylab = "Weighted Score", xlab = "Job Posting", main = paste("Weighted Scores Across 10 jobs for",Student))
-    box()
+    
     
     #avg weighted score for job postings of each skill
 progQ<-sprintf("SELECT AVG( Weight_Score )
@@ -41,10 +39,9 @@ techQ <- str_replace_all(str_replace_all(techQ,"\n",""),"\\s+"," ")
 techScore <- dbGetQuery(mydb,techQ)
     
     skillScores <- data.frame(Programming = progScore[1,], Statistics = StatScore[1,], Technical_Design =techScore[1,])
-    barplot(height = as.numeric(skillScores[1,]), names.arg = names(skillScores), ylim=c(0,100),ylab = "Average Weighted Score", xlab = "Skill", main = "Average Score for each skill across all postings")
-    box()
-    
-      ##AVG Student ratings by company
+
+
+  ##AVG Student ratings by company
     review_Q<-sprintf("SELECT R.Employer_Email, AVG( R.Overall_Rating ) , E.Industry
     FROM Company_Rating R, Employer E
     GROUP BY R.Employer_Email")
@@ -55,8 +52,22 @@ techScore <- dbGetQuery(mydb,techQ)
     ##OBTAINED FROM: https://stackoverflow.com/questions/10286473/rotating-x-axis-labels-in-r-for-barplot/21978596
     rotate_x <- function(data, column_to_plot, labels_vec, rot_angle) {
         plt <- barplot(data[[column_to_plot]], col='steelblue', xaxt="n",ylim = c(0,10),ylab = "Average Review Score", main = "Employer Review Scores")
-        text(plt, par("usr")[3], labels = labels_vec, srt = rot_angle, adj = c(1.1,1.1), xpd = TRUE, cex=0.6) 
+        text(plt, par("usr")[3], labels = labels_vec, srt = rot_angle, adj = c(1.1,1.1), xpd = TRUE, cex=0.55) 
     }
+   
+    
+    layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
+    
     rotate_x(reviews,'AVG_Score', labels_vec = reviews$Employer_Email, rot_angle = 65)
-
-}
+    box()
+    barplot(height = as.numeric(skillScores[1,]), col='steelblue',names.arg = c("Programming","Statistics","Design"), ylim=c(0,100),ylab = "Average Weighted Score", xlab = "Skill", main = "Average Score for each skill \nacross all postings")
+    box()
+    barplot(height = studentInfo$Weight_Score, col='steelblue',names = studentInfo$Opportunity_ID,ylim=c(0,120),ylab = "Weighted Score", xlab = "Job Posting", main = paste("Weighted Scores Across Top 10 jobs \nfor",Student))
+    box()
+  
+    ##Obtained from https://www.codeproject.com/Articles/1119237/Pass-Arguments-and-Execute-R-script-from-PHP-Forms
+    png(filename="C:/Users/justi/OneDrive/Documents/R/student_graphs.png", width=500, height=500)
+    dev.off()
+    sink('analysis-output.txt', append=FALSE, type = c("output", "message"))
+    
+    }
