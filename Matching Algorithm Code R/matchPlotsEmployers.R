7897ee998aad4446f902a965f1e46d17be4ba41f
smit3400 <56548125+smit3400@users.noreply.github.com>
2020-11-29 20:30:43 -0500

    Update matchPlotsEmployers.R

--- Matching Algorithm Code R/matchPlotsEmployers.R
+++ Matching Algorithm Code R/matchPlotsEmployers.R
@@ -2,7 +2,7 @@ require("caret")
 library(caret)
 library(e1071)
 library(ROCR)
-matchPlotEmployer <- function(mydb)
+matchPlotsEmployers <- function(mydb)
 {
 
   Q1<-sprintf("SELECT O.* , AVG( Weight_Score )
@@ -30,7 +30,7 @@ matchPlotEmployer <- function(mydb)
   predictions <- cbind(testing,predicted_Weighted_Score)
   predictions<- predictions[,-8]
   
- par(mfrow = c(2,1))
+
   
   Q2<-sprintf("SELECT `Opportunity_ID` , AVG( `Weight_Score` )
   FROM `Match_Score`
@@ -39,15 +39,22 @@ matchPlotEmployer <- function(mydb)
   Q2 <- str_replace_all(str_replace_all(Q2,"\n",""),"\\s+"," ")
   rankID <- dbGetQuery(mydb,Q2)
   names(rankID)[names(rankID) == "AVG( `Weight_Score` )"] <- "AVG_Score"
-  barplot(height = rankID$AVG_Score, names.arg = rankID$Opportunity_ID, ylim = c(0,100) ,ylab = "Average Score", xlab = "Job Posting #", main = "Average Score Across Top 10 Postings for All Companies")
-  box()
+  
   
   Q3 <- sprintf("SELECT `Statistics_Skill` , `Programming_Skill` , `Technical_Design_Skill`
  FROM `Student")
   sCounts<- dbGetQuery(mydb, Q3)
   skills <- colnames(sCounts)[apply(sCounts,1,which.max)]
   df <- data.frame(Statistics = sum(skills == "Statistics_Skill"), Programming = sum(skills == "Programming_Skill"), Design = sum(skills == "Technical_Design_Skill") )
+  
+  png(filename="C:/Users/justi/OneDrive/Documents/R/employer_graphs.png", width=500, height=500)
+  par(mfrow=c(2,1))
+  barplot(height = rankID$AVG_Score, names.arg = rankID$Opportunity_ID, ylim = c(0,100) ,ylab = "Average Score", xlab = "Job Posting #", main = "Average Score Across Top 10 Postings for All Companies")
+  box()
   pie(as.numeric(df[1,]), names(sCounts),main = "Student's Highest Ranking Skills")
+  
+  dev.off()
+  sink('analysis-output.txt', append=FALSE, type = c("output", "message"))
   
   return(predictions)  
 }
