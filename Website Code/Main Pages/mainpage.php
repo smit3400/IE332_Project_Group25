0f84e58d86d2b37caf30ff5399e1fb54820448e6
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 02:16:53 -0500

    Create mainpage.php

---
+++ Website Code/Main Pages/mainpage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>IE Connect</title>
+    <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1116905/main/ie.ico"/>
+    <link href="test21.css" width = "100%" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <header class="h1">
+      <div class="left-side">
+        <img class="banner3" src="https://www.cco.purdue.edu/Content/Layout/logo.svg" style= "width:250px">
+        <hr class="divider">
+        <div class="page-title">
+          <p class="title-main">IE Connect</p>
+          <p class="title">Simplifying the recruiting process</p>
+        </div>
+      </div>
+      <img class="banner1" src="https://cdn.shopify.com/s/files/1/0241/9737/products/1008-PUR-Tank-black_2_1800x.jpg?v=1571442802" style= "width:100px">
+    </header>
+    <div class="page">
+      <img class="hand" src="https://image.freepik.com/free-vector/hand-drawn-golden-handshake_53562-9984.jpg">
+      <div class="person-options">
+        <div class="button">
+          <a href = "student/student_login.php" target="_self">I am a Student</a>
+        </div>
+        <div class="button">
+          <a href="employer/employer_login.php" target="_self">I am an Employer</a>
+        </div>
+        <div class="button">
+          <a href="advisor/IE_login.php" target="_self">I am an IE Advisor</a>
+        </div>
+	<h2><p><a href="member_page.php" class = "button1">IE 332 Team 25 Members</a></p></h2>
+  </body>
+</html>
