9178ccf946c6e2d3abea3a7f83359a8d5080ca9b
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 02:17:46 -0500

    Create member_page.php

---
+++ Website Code/Main Pages/member_page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="member_css.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<header class="h1">
+      <div class="left-side">
+        <img class="banner3" src="https://www.cco.purdue.edu/Content/Layout/logo.svg" style= "width:250px">
+        <hr class="divider">
+        <div class="page-title">
+          <p class="title-main">IE Connect</p>
+          <p class="title">Simplifying the recruiting process</p>
+        </div>
+      </div>
+      <img class="banner1" src="https://cdn.shopify.com/s/files/1/0241/9737/products/1008-PUR-Tank-black_2_1800x.jpg?v=1571442802" style= "width:100px">
+    </header>
+	<h1>Meet the Team!!</h1>
+	
+	<!-- how to use image with URL retrieved from https://www.w3schools.com/html/html_images.asp-->
+	
+	<p><img src="https://i.ibb.co/qCMM0Nb/group-pic.png" width="1000" height="400"alt="group-pic" border="0"></p>
+	
+	
+<p><a href="mainpage.php" class = "button1">Main Page</a></p>
+
+</body>
+</html>
