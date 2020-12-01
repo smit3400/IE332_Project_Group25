4280b01046efbc4efdad3ba15ffd40345afcb8b6
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 01:58:30 -0500

    Update student_logout.php

--- Website Code/Student Pages/student_logout.php
+++ Website Code/Student Pages/student_logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-unset($_SESSION['id']);
-session_destroy();
+
+//setting variable to use in all other pages so student cannot use back arrow in browser after logging out
+$_SESSION["log"] = 0;
+unset($_SESSION["stud_email"]);
 header("Location: student_login.php");
 ?>
