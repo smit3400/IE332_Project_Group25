d037f5b196bf5206f5f9d47284ab17f4d89c3aa9
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 02:02:29 -0500

    Update employer_logout.php

--- Website Code/Employer Pages/employer_logout.php
+++ Website Code/Employer Pages/employer_logout.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
+
+$_SESSION["emp_log"] = 0;
+
+//End php session
 unset($_SESSION['id']);
 session_destroy();
+
+//Redirect to login page
 header("Location: employer_login.php");
 ?>
