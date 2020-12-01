17d65f5d2cd6fd7f673e49d35a430ae7d32e2445
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 02:01:56 -0500

    Update employer_form_submit.php

--- Website Code/Employer Pages/employer_form_submit.php
+++ Website Code/Employer Pages/employer_form_submit.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html>
-<body>
-	Welcome <?php echo $_GET["company"]; ?><br>
-	Your email address is: <?php echo $_GET["email"]; ?> <br>
-	<?php
 
-	echo "Database Connection" . "<br>" . "<br>";
+<!-- CSS Connection -->
+<head>
+	<title>IE Connect</title>
+    <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1116905/main/ie.ico"/>
+    <link href="employerForm.css" rel="stylesheet" type="text/css" />
+</head>
+
+<!-- Website header -->
+<body>
+    <header class="h1">
+      <div class="left-side">
+        <img class="banner3" src="https://www.cco.purdue.edu/Content/Layout/logo.svg" style= "width:250px">
+        <hr class="divider">
+        <div class="page-title">
+          <p class="title-main">IE Connect</p>
+          <p class="title">Simplifying the recruiting process</p>
+        </div>
+      </div>
+      <img class="banner1" src="https://cdn.shopify.com/s/files/1/0241/9737/products/1008-PUR-Tank-black_2_1800x.jpg?v=1571442802" style= "width:100px">
+    </header>
+
+    <!-- Welcome banner -->
+	<h1>Welcome <?php echo $_POST["company"]; ?></h1>
+	<p>Your email address is: <?php echo $_POST["email"]; ?></p>
+	
+	<?php
+	if ($_POST["email"] == "") {
+		header("Location:  employer_login.php");
+	}
+
 	//Connecting to database
 	$servername = "mydb.itap.purdue.edu";
 	$username = "g1116905";
@@ -17,33 +42,31 @@
 	if (mysqli_connect_errno()) {
 	    printf("Connect failed: %s\n", mysqli_connect_error());
 	    exit();
-	}else{
-		echo "Connected Successfully!" . "<br>";
 	}
 
-	echo "Inserting Data". "<br>" . "<br>";
+	//Pulling variables from form submission
+	$email = $_POST["email"];
+	$pass = $_POST["password"];
+	$company = $_POST["company"];
+	$phone = $_POST["phone"];
+	$description = $_POST["description"];
+	$industry = $_POST["industry"];
 
-	$email = $_GET["email"];
-	$pass = $_GET["password"];
-	$company = $_GET["company"];
-	$phone = $_GET["phone"];
-	$description = $_GET["description"];
-	$industry = $_GET["industry"];
-
+	//Inserting data into sql database
 	$sql = "INSERT INTO Employer(Email, Password, Company_Name, Phone_Number, Description, Industry)
 	VALUES('" . $email . "', '" . $pass . "', '" . $company . "', '" . $phone . "', '" . $description . "', '" . $industry . "')";
 
 	if (mysqli_query($data_base, $sql)) {
-	  echo "New record created successfully";
+	  echo "<p>Please login below to see your account!</p>";
 	} else {
 	  echo "Error: " . $sql . "<br>" . mysqli_error($data_base);
 	}
 
+	//Closing connection
 	mysqli_close($data_base);
 
 	?>
-	<p><a href = "employer_login.php">Login Page</a></p>
-
+	<p><a href = "employer_login.php" class = "button1">Login Page</a></p>
 
 </body>
 </html>
