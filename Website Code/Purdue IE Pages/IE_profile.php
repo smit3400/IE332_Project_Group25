b45107d5ad0a60ac969fc87d2f8fc7fffb9a8aaf
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 02:11:10 -0500

    Update IE_profile.php

--- Website Code/Purdue IE Pages/IE_profile.php
+++ Website Code/Purdue IE Pages/IE_profile.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+if ($_SESSION["ad_log"] == 0) {
+	header("Location: IE_login.php");
+}
+
 //Connecting to database
 $servername = "mydb.itap.purdue.edu";
 $username = "g1116905";
@@ -12,14 +16,11 @@ $data_base = mysqli_connect($servername, $username, $password, $dbname);
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
-}else{
-	echo "Connected Successfully!" . "<br>";
 }
 
 //Pulling info from sql
 $sql = "SELECT * FROM Purdue_IE WHERE Email='" . $_SESSION["email"] . "'";
 if ($result = mysqli_query($data_base,$sql)) {
-    printf("Select returned %d rows. <br><br>", mysqli_num_rows($result));
     /* free result set */
     while ($row=mysqli_fetch_row($result))
     {
@@ -30,15 +31,44 @@ if ($result = mysqli_query($data_base,$sql)) {
 	  // Free result set
 	  //mysqli_free_result($result);
 }
-printf ("IE Email:<br>%s <br><br>", $IE_email);
-printf ("First Name:<br>%s <br><br>", $IE_fname);
-printf ("Last Name:<br>%s <br><br>", $IE_lname);
 ?>
 
 <!DOCTYPE html>
 <html>
-<body>
 
-<p><a href = "IE_main.php">Main Page</a></p>
+<!-- calling in css external file -->
+
+<head>
+	<title>IE Connect</title>
+    <link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1116905/main/ie.ico"/>
+    <link href="advisorForm.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <header class="h1">
+      <div class="left-side">
+        <img class="banner3" src="https://www.cco.purdue.edu/Content/Layout/logo.svg" style= "width:250px">
+        <hr class="divider">
+        <div class="page-title">
+          <p class="title-main">IE Connect</p>
+          <p class="title">Simplifying the recruiting process</p>
+        </div>
+      </div>
+      <img class="banner1" src="https://cdn.shopify.com/s/files/1/0241/9737/products/1008-PUR-Tank-black_2_1800x.jpg?v=1571442802" style= "width:100px">
+    </header>
+	
+	<h1>IE Advisor Profile</h1>
+
+
+
+	<?php
+	
+	//printing information for profile page from DB
+	printf ("<p><b>IE Email:</b> %s </p>", $IE_email);
+	printf ("<p><b>First Name:</b> %s </p>", $IE_fname);
+	printf ("<p><b>Last Name:</b> %s </p>", $IE_lname);
+	?>
+
+<p><a href = "IE_update.php" class = "button1">Update Profile</a></p>
+<p><a href="IE_main.php" class = "button1">Main Page</a></p>
 </body>
 </html>
