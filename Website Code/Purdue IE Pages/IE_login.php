214ab52d3fd7e08f1693904590b185f2436597e2
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 02:10:20 -0500

    Update IE_login.php

--- Website Code/Purdue IE Pages/IE_login.php
+++ Website Code/Purdue IE Pages/IE_login.php
@@ -2,22 +2,31 @@
 
 session_start();
 
+if ($_SESSION["ad_log"] == 1) {
+	header("Location: IE_main.php");
+}
+
 $message="";
 if(count($_POST)>0) {
+  
+  //connecting to DB
   $servername = "mydb.itap.purdue.edu";
   $username = "g1116905";
   $password = "iegroup25";
   $dbname = "g1116905";
 
+	//authenticating login credentials
   
   $conn = mysqli_connect($servername, $username, $password, $dbname);
-  $result = mysqli_query($conn,"SELECT * FROM Purdue_IE WHERE Email='" . $_POST["Email"] . "'");
+  $result = mysqli_query($conn,"SELECT * FROM Purdue_IE WHERE Email='" . $_POST["Email"] . "' AND Password = '". $_POST["Password"]."'");
   $count  = mysqli_num_rows($result);
   if($count==0) {
-    $message = "Invalid Username or Password!";
+    $message = "Invalid username or password!";
   } else {
     $message = "You are successfully authenticated!";
     header("Location: IE_main.php");
+
+	$_SESSION["ad_log"] = 1;
 
     $_SESSION["email"] =  $_POST["Email"];
 
@@ -29,24 +38,51 @@ if(count($_POST)>0) {
 
 <!DOCTYPE html>
 <html>
-<body>
 
-<h2>Hello Purdue IE</h2>
+<!-- calling in css external file -->
 
-<h4>Login</h4>
-<form action = "" method = "post">
-  <label>Email:</label><br>
-  <input type="email" name="Email" placeholder="Email address" required>
-  <br>
-  <label>Password:</label><br>
-  <input type="password" name="Password" placeholder="Password" required>
-  <br>
-  <br>
-  <input type="submit" value="Sign in">
-</form>
-
-<p> New user? Create an account <a href = "IE_form.php">here</a></p>
-<div class="message"><?php if($message!="") { echo $message; } ?></div>
-
-</body>
+	<head>
+		<title>IE Connect</title>
+		<link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1116905/main/ie.ico"/>
+		<link href="login.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<img class="logo" src="https://wl.mypurdue.purdue.edu/static_resources/portal/images/logo.png">
+		<div class="login-container">
+			<h2 class="title">Welcome IE Advisor!</h2>
+			<form action="" method = "post">
+				<div class="form">
+					<label for="Email">Email:</label>
+				    <input class="input" type="email" id="Email" name="Email" placeholder="Email address" required>
+				</div>
+			    <br>
+			    <div class="form">
+			    	<label for="Password">Password:</label>
+				    <input class="input" type="password" id="Password" name="Password" placeholder="Password" required>
+					<input type="checkbox" onclick="myFunction()">Show Password
+					<script>
+					
+					// function to see password 
+					
+					function myFunction() {
+						var x = document.getElementById("Password");
+						if (x.type === "password") {
+							x.type = "text";
+						} else {
+							x.type = "password";
+						}
+					}
+					</script>
+			    </div>
+			    <br>
+				<div class="message"><?php if($message!="") { echo $message; } ?></div>
+			    <br>
+				<!-- submission will go to main -->
+			    <input class="sign-in" type="submit" value="Sign in">
+			</form>
+			<p> New user? Create an account below!</p>
+			<p><a class="new-account" href="IE_form.php" target="_self" >Create account</a></p>
+			<p><a class="new-account" href="https://web.ics.purdue.edu/~g1116905/main/mainpage.php" target="_self" >Home page</a></p>
+		</div>
+	</body>
 </html>
