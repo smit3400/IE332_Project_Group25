f4b6145e143cc2ea844b54b3d4c0ae9a85507575
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 02:02:17 -0500

    Update employer_login.php

--- Website Code/Employer Pages/employer_login.php
+++ Website Code/Employer Pages/employer_login.php
@@ -1,6 +1,9 @@
 <?php
-
 session_start();
+
+if ($_SESSION["emp_log"] == 1) {
+	header("Location: employer_main.php");
+}
 
 $message="";
 if(count($_POST)>0) {
@@ -9,15 +12,19 @@ if(count($_POST)>0) {
   $password = "iegroup25";
   $dbname = "g1116905";
 
-  
+  //Database connection
   $conn = mysqli_connect($servername, $username, $password, $dbname);
-  $result = mysqli_query($conn,"SELECT * FROM Employer WHERE Email='" . $_POST["Email"] . "'");
+  
+  //Email and password verification
+  $result = mysqli_query($conn,"SELECT * FROM Employer WHERE Email='" . $_POST["Email"] . "' AND Password ='" . $_POST["Password"] . "'");
   $count  = mysqli_num_rows($result);
   if($count==0) {
     $message = "Invalid Username or Password!";
   } else {
     $message = "You are successfully authenticated!";
     header("Location: employer_main.php");
+
+	$_SESSION["emp_log"] = 1;
 
     $_SESSION["email"] =  $_POST["Email"];
 
@@ -27,26 +34,54 @@ if(count($_POST)>0) {
 }
 ?>
 
+
 <!DOCTYPE html>
 <html>
-<body>
+<!-- CSS Connection -->
+	<head>
+		<title>IE Connect</title>
+		<link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1116905/main/ie.ico"/>
+		<link href="login.css" rel="stylesheet" type="text/css" />
+	</head>
 
-<h2>Hello Employer!</h2>
-
-<h4>Login</h4>
-<form action = "" method = "post">
-  <label>Email:</label><br>
-  <input type="email" name="Email" placeholder="Email address" required>
-  <br>
-  <label>Password:</label><br>
-  <input type="password" name="Password" placeholder="Password" required>
-  <br>
-  <br>
-  <input type="submit" value="Sign in">
-</form>
-
-<p> New user? Create an account <a href = "employer_form.php">here</a></p>
-<div class="message"><?php if($message!="") { echo $message; } ?></div>
-
-</body>
+	<!-- Website header -->
+	<body>
+		<img class="logo" src="https://wl.mypurdue.purdue.edu/static_resources/portal/images/logo.png">
+		<div class="login-container">
+			<h2 class="title">Welcome Employer!</h2>
+			
+			<!-- Login Form -->
+			<form action="" method = "post">
+				<div class="form">
+					<label for="Email">Email:</label>
+				    <input class="input" type="email" id="Email" name="Email" placeholder="Email address" required>
+				</div>
+			    <br>
+			    <div class="form">
+			    	<label for="Password">Password:</label>
+				    <input class="input" type="password" id="Password" name="Password" placeholder="Password" required>
+					
+					<!-- Show password checkbox -->
+					<input type="checkbox" onclick="myFunction()">Show Password
+					<script>
+					function myFunction() {
+						var x = document.getElementById("Password");
+						if (x.type === "password") {
+							x.type = "text";
+						} else {
+							x.type = "password";
+						}
+					}
+					</script>
+			    </div>
+			    <br>
+				<div class="message"><?php if($message!="") { echo $message; } ?></div>
+			    <br>
+			    <input class="sign-in" type="submit" value="Sign in">
+			</form>
+			<p> New user? Create an account below!</p>
+			<p><a class="new-account" href="employer_form.php" target="_self" >Create account</a></p>
+			<p><a class="new-account" href="https://web.ics.purdue.edu/~g1116905/main/mainpage.php" target="_self" >Home page</a></p>
+		</div>
+	</body>
 </html>
