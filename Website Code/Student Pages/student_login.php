c8f9d28110f34a9d4dab91c78bd150839e884f8a
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-01 01:58:15 -0500

    Update student_login.php

--- Website Code/Student Pages/student_login.php
+++ Website Code/Student Pages/student_login.php
@@ -1,6 +1,9 @@
 <?php
-
 session_start();
+
+if ($_SESSION["log"] == 1) {
+	header("Location: student_main.php");
+}
 
 $message = "";
 
@@ -15,10 +18,11 @@ if(count($_POST)>0) {
 	$result = mysqli_query($conn,"SELECT * FROM Student WHERE Email='" . $_POST["stud_email"] . "' AND Password = '". $_POST["stud_password"]."'");
 	$count  = mysqli_num_rows($result);
 	if($count==0) {
-		$message = "Invalid Username or Password!";
+		$message = "Invalid username or password!";
 	} else {
-		$message = "You are successfully authenticated!";
 		header("Location: student_main.php");
+		
+		$_SESSION["log"] = 1;
 
 		$_SESSION["stud_email"] =  $_POST["stud_email"];
 	}
@@ -29,8 +33,11 @@ if(count($_POST)>0) {
 
 <!DOCTYPE html>
 <html>
+<!-- importing css from external file-->
 	<head>
-		<link href="login.css" rel="stylesheet" type="text/css" />
+		<title>IE Connect</title>
+		<link rel="icon" type="image/ico" href="https://web.ics.purdue.edu/~g1116905/main/ie.ico"/>
+		<link href="login.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<img class="logo" src="https://wl.mypurdue.purdue.edu/static_resources/portal/images/logo.png">
@@ -45,14 +52,27 @@ if(count($_POST)>0) {
 			    <div class="form">
 			    	<label for="stud_password">Password:</label>
 				    <input class="input" type="password" id="stud_password" name="stud_password" placeholder="Password" required>
+					<input type="checkbox" onclick="myFunction()">Show Password
+					<script>
+					<!-- java function for showing password after checking box-->
+					function myFunction() {
+						var x = document.getElementById("stud_password");
+						if (x.type === "password") {
+							x.type = "text";
+						} else {
+							x.type = "password";
+						}
+					}
+					</script>
 			    </div>
 			    <br>
+				<div class="message"><?php if($message!="") { echo $message; } ?></div>
 			    <br>
 			    <input class="sign-in" type="submit" value="Sign in">
 			</form>
 			<p> New user? Create an account below!</p>
 			<p><a class="new-account" href="student_form.php" target="_self" >Create account</a></p>
+			<p><a class="new-account" href="https://web.ics.purdue.edu/~g1116905/main/mainpage.php" target="_self" >Home page</a></p>
 		</div>
-		<div class="message"><?php if($message!="") { echo $message; } ?></div>
 	</body>
 </html>
