8076cf56f9704e9a45ece8a3fd04eeac3e80f06a
juliefrazz <72420956+juliefrazz@users.noreply.github.com>
2020-12-02 14:03:47 -0500

    Update matchPlotsEmployers.R

--- Matching Algorithm Code R/matchPlotsEmployers.R
+++ Matching Algorithm Code R/matchPlotsEmployers.R
@@ -1,35 +1,15 @@
-require("caret")
-library(caret)
-library(e1071)
-library(ROCR)
-matchPlotsEmployers <- function(mydb)
-{
+require("RMySQL")
+require("data.table")
+require("stringr")
 
-  Q1<-sprintf("SELECT O.* , AVG( Weight_Score )
- FROM Match_Score M, Opportunities O
- WHERE M.Opportunity_ID = O.Opportunity_ID
- GROUP BY Opportunity_ID")
-  Q1 <- str_replace_all(str_replace_all(Q1,"\n",""),"\\s+"," ")
-  print(Q1)
-  OScores <- dbGetQuery(mydb,Q1)
+##Connect to the database
+mydb <- dbConnect(MySQL(), user = "g1116905", password = "iegroup25", dbname = "g1116905", host = "mydb.itap.purdue.edu")
+on.exit(dbDisconnect(mydb))
 
-  OScores <- OScores[,-7]
-  OScores <- OScores[,-7]
-  OScores <- OScores[,-1]
-  OScores <- OScores[,-1]
-  names(OScores)[names(OScores) == "AVG( Weight_Score )"] <- "AVG_Score"
- #ML Algorithm - NB to predict average matchscore based off of job information
-  indexTrain <- createDataPartition(y = OScores$AVG_Score,p = 0.5,list = FALSE)
-  training <- OScores[indexTrain,]
-  testing <- OScores[-indexTrain,]
+args <- commandArgs(TRUE)
 
-  model<-train(AVG_Score~., data = training,  method = "knn",
-               preProcess = c('scale', 'center'),
-               na.action = na.omit)
-  predicted_Weighted_Score <- predict(model, newdata = testing)
-  predictions <- cbind(testing,predicted_Weighted_Score)
-  predictions<- predictions[,-8]
-  
+filename <- args[1]
+filename <- paste(filename,"png",sep=".")
 
   
   Q2<-sprintf("SELECT `Opportunity_ID` , AVG( `Weight_Score` )
@@ -47,7 +27,8 @@ matchPlotsEmployers <- function(mydb)
   skills <- colnames(sCounts)[apply(sCounts,1,which.max)]
   df <- data.frame(Statistics = sum(skills == "Statistics_Skill"), Programming = sum(skills == "Programming_Skill"), Design = sum(skills == "Technical_Design_Skill") )
   
-  png(filename="C:/Users/justi/OneDrive/Documents/R/employer_graphs.png", width=500, height=500)
+  png(filename=paste0("/home/campus/g1116905/www/main/Project_R/plots/",filename), width=500, height=500)
+
   par(mfrow=c(2,1))
   barplot(height = rankID$AVG_Score, names.arg = rankID$Opportunity_ID, ylim = c(0,100) ,ylab = "Average Score", xlab = "Job Posting #", main = "Average Score Across Top 10 Postings for All Companies")
   box()
@@ -55,6 +36,3 @@ matchPlotsEmployers <- function(mydb)
   
   dev.off()
   sink('analysis-output.txt', append=FALSE, type = c("output", "message"))
-  
-  return(predictions)  
-}
